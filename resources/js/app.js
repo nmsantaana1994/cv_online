e1a811836983fafb5df651f84e87e84f4e4282a9
Nicolás Santa Ana <nmsantaana1994@gmail.com>
2025-07-20 22:28:40 -0300

    Init: Laravel10 + Auth + Vite/Vue + PWA setup

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import { createApp } from "vue";
+
+// Si luego agregas componentes, se registran aquí:
+// import Example from './components/Example.vue';
+// const app = createApp({});
+// app.component('example', Example);
+// app.mount('#app-vue');
